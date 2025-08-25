101f8ed2785aba3101cb6b95b828d2c0abb9c886
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2025-08-25 11:58:50 -0300

    Removing custom zod error handler on fetcher

--- dist/esm/fetcher.js
+++ dist/esm/fetcher.js
@@ -15,13 +15,13 @@ export async function fetcher(url, { method = "GET", headers = {}, params, body,
     const isJson = contentType.includes("application/json");
     const data = isJson ? await res.json() : await res.text();
     if (!res.ok) {
-        if (Array.isArray(data?.error)) {
-            const err = data?.error.reduce((acc, curr) => {
-                acc[curr.path.at(0)] = curr.code;
-                return acc;
-            }, {});
-            throw new Error(JSON.stringify(err));
-        }
+        // if (Array.isArray(data?.error)) {
+        //   const err = data?.error.reduce((acc: any, curr: any) => {
+        //     acc[curr.path.at(0)] = curr.code;
+        //     return acc;
+        //   }, {});
+        //   throw new Error(JSON.stringify(err));
+        // }
         throw new Error(data?.message || data?.error || `Request failed with status ${res.status}`);
     }
     return data;
